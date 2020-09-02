05737d6faf6272f7f9effc9ec4180139203d7bcd
themohal <farjadmohal@gmail.com>
2020-09-02 18:29:57 +0500

    commit

--- functions/connect.js
+++ functions/connect.js
@@ -1,7 +1,21 @@
 const mongoose =  require('mongoose');
+const express = require('express');
+const app = express();
+const Post = require('../../mean-app/backend/models/post');
+const path = require('path');
 
+
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(express.static(__dirname + '/dist'));
+let controller = require('../backend/controller');
 exports.handler = function(event, context, callback) {
-  let dbName = "mean-app";
+
+
+
+let dbName = "mean-app";
 let password = "zbryPOKORHLa5C4J";
 mongoose.connect("mongodb+srv://phoenix:"+password+"@phoenix-cluster.24xxp.mongodb.net/"+dbName+"?retryWrites=true&w=majority",{useUnifiedTopology: true, useNewUrlParser: true ,useFindAndModify:false})
 .then(()=>{
@@ -10,6 +24,11 @@ console.log("Connected to database");
 }).catch(()=>{
   console.log("Connection Failed");
 }).finally(()=>{
-mongoose.disconnect();
+//mongoose.disconnect();
 });
+
+app.use('/api',controller);
+
+
+module.exports = app; //to import it in server.js where node server is running
 }
